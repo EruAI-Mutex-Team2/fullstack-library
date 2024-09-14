7193f4832978fcccd2f8a65686139e2e1253c400
1beyzaslan <beyza_aslan_8915@hotmail.com>
2024-09-14 21:42:49 +0300

    login page eklendi

--- libraryApp.frontend/src/App.jsx
+++ libraryApp.frontend/src/App.jsx
@@ -1,13 +1,17 @@
+import Login from "./components/Login";
 
 function App() {
-
   return (
-    <>
-      <div>
-        <h1>TEST</h1>
+    <div className="flex w-full h-screen">
+      <div className="w-full flex items-center justify-center lg:w-1/2">
+        <Login />
       </div>
-    </>
-  )
+      <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200 ">
+        <div className="w-60 h-60 bg-gradient-to-tr from-violet-300 to-blue-950 rounded-full animate-pulse"/>
+        <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg "/>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
