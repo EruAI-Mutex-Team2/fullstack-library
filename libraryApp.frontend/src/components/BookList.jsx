38170d3ad81b18ae11bb8cc279e8024052bd2fd9
Ezgi <ezgycl28@gmail.com>
2024-09-19 14:54:04 +0300

    add book searching page

---
+++ libraryApp.frontend/src/components/BookList.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+export default function BookList() {
+  return (
+    <div>
+       <div className='pl-6 pr-6 pt-6'>
+      <div className="flex justify-between items-center bg-purple-500 text-white p-4 rounded-md shadow-lg mb-6 ">
+        <h1 className="text-3xl font-bold">Book List</h1>
+        <div className="flex">
+          <input
+            id="book-search"
+            className="p-2 border border-gray-300 rounded-lg shadow-sm"
+            type="text"
+            placeholder="Search for a book..."
+          />
+          <button className="bg-purple-700 text-white py-2 px-4 ml-2 rounded-lg shadow-lg">
+            Search
+          </button>
+        </div>
+      </div>
+      </div>
+    
+      <div className='pl-10 pr-10'>
+      <div className="bg-white shadow-lg rounded-lg p-6">
+        <table className="min-w-full">
+          <thead>
+            <tr className="bg-purple-600 text-white">
+              <th className="p-6 text-left">Title</th>
+              <th className="p-6 text-left">Publication Date</th>
+              <th className="p-6 text-left">Is Borrowed</th>
+              <th className="p-6 text-left">Actions</th>
+            </tr>
+          </thead>
+
+          
+          <tbody>
+
+
+            <tr className="border-b">
+              <td className="p-6">Example Book</td>
+              <td className="p-6">01/01/2024</td>
+              <td className="p-6">Yes</td>
+              <td className="p-6">
+                <button className="bg-blue-500 text-white py-1 px-2 rounded-lg mr-2">
+                Preview
+                </button>
+                <button className="bg-green-500 text-white py-1 px-2 rounded-lg">
+                  Borrow
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      </div>
+    </div>
+  );
+}
