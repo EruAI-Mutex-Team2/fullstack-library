a253698ef41e03b1ebebfff450b343a308f22ca7
Ezgi <ezgycl28@gmail.com>
2024-09-27 12:44:45 +0300

    update register page

--- libraryApp.frontend/src/components/Register.jsx
+++ libraryApp.frontend/src/components/Register.jsx
@@ -2,81 +2,83 @@ import * as React from 'react';
 
 export default function Register() {
   return (
-    <div className='bg-white px-10 py-20 rounded-3xl border-2 border-gray-50 max-w-lg mx-auto'>
-    
-      <h1 className='text-4xl font-bold text-center text-gray-800'>
-        Create an Account
-      </h1> 
-
-      <p className='font-medium text-lg text-gray-500 mt-4 text-center'>
-        Please fill in your details to create a new account.
-      </p>
-
-      <div className='mt-8' >
-
-        <div>
-          <label className='text-base font-medium'>First Name</label>
-          <input
-            className='w-full h-1 border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
-            placeholder='Enter your first name'
-          />
-        </div>
+    <div className="flex w-full h-screen">
 
 
-        <div className='mt-4'>
-          <label className='text-base font-medium'>Last Name</label>
-          <input
-            className='w-full h-1 border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
-            placeholder='Enter your last name'
-          />
-        </div>
+      <div className="w-full lg:w-1/2 flex items-center justify-center">
+        <div className='bg-white px-10 py-20 rounded-3xl border-2 border-gray-50 max-w-lg mx-auto'>
+          <h1 className='mt-3 text-4xl font-bold text-center text-gray-800'>
+            Create an Account
+          </h1> 
 
+          <p className='font-medium text-lg text-gray-500 mt-4 text-center'>
+            Please fill in your details to create a new account.
+          </p>
 
-        <div className='mt-4'>
-          <label className='text-base font-medium'>Email</label>
-          <input
-            className='w-full h-1 border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
-            placeholder='Enter your email'
-          />
-        </div>
+          <div className='mt-8'>
+            <div>
+              <label className='text-base font-medium'>First Name</label>
+              <input
+                className='w-full h-12 border border-gray-300 rounded-xl p-4 mt-1 bg-transparent'
+                placeholder='Enter your first name'
+              />
+            </div>
 
+            <div className='mt-4'>
+              <label className='text-base font-medium'>Last Name</label>
+              <input
+                className='w-full h-12 border border-gray-300 rounded-xl p-4 mt-1 bg-transparent'
+                placeholder='Enter your last name'
+              />
+            </div>
 
+            <div className='mt-4'>
+              <label className='text-base font-medium'>Email</label>
+              <input
+                className='w-full h-12 border border-gray-300 rounded-xl p-4 mt-1 bg-transparent'
+                placeholder='Enter your email'
+              />
+            </div>
 
-        <div className='mt-4'>
-          <label className='text-base font-medium'>Password</label>
-          <input
-            className='w-full h-1 border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
-            placeholder='Enter your password'
-            type='password'
-          />
-        </div>
+            <div className='mt-4'>
+              <label className='text-base font-medium'>Password</label>
+              <input
+                className='w-full h-12 border border-gray-300 rounded-xl p-4 mt-1 bg-transparent'
+                placeholder='Enter your password'
+                type='password'
+              />
+            </div>
 
+            <div className='mt-4'>
+              <label className='text-base font-medium'>Password Confirm</label>
+              <input
+                className='w-full h-12 border border-gray-300 rounded-xl p-4 mt-1 bg-transparent'
+                placeholder='Confirm your password'
+                type='password'
+              />
+            </div>
 
-        <div className='mt-4'>
-          <label className='text-base font-medium'>Password Confirm</label>
-          <input
-            className='w-full h-1 border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
-            placeholder='Enter your password'
-            type='password'
-          />
-        </div>
+            <div className='mt-8 flex flex-col gap-y-4'>
+              <button className='py-3 rounded-xl bg-violet-500 text-white text-lg font-bold'>
+                Sign up
+              </button>
+            </div>
 
-
-        <div className='mt-8 flex flex-col gap-y-4'>
-          <button className='py-3 rounded-xl bg-violet-500 text-white text-lg font-bold'>
-            Sign up
-          </button>
-        </div>
-
-
-
-        <div className='mt-8 flex justify-center items-center'>
-          <p className='font-medium text-base'>Already have an account?</p>
-          <button className='text-violet-500 text-base font-medium ml-2'>
-            Sign in
-          </button>
+            <div className='mt-8 flex justify-center items-center'>
+              <p className='font-medium text-base'>Already have an account?</p>
+              <button className='text-violet-500 text-base font-medium ml-2'>
+                Sign in
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div> 
+
+      
+      <div className="hidden lg:flex w-1/2 items-center justify-center relative bg-gray-200">
+        <div className="w-60 h-60 bg-gradient-to-tr from-violet-300 to-blue-950 rounded-full animate-pulse" />
+        <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg" />
+      </div>
+    </div>
   );
 }
