e79787792993adfcf90e9a2840cc91d5a54483fd
melekaltun <melekaltun8035@gmail.com>
2024-09-28 15:46:37 +0300

    account controller

---
+++ libraryApp.backend/Dtos/kayitOldto.cs
@@ -0,0 +1,8 @@
+public class kayitOldto
+{
+    public static string Isim { get; set; }
+    public static string SoyIsim { get; set; }
+    public static string Email { get; set; }
+    public static string Password { get; set; }
+
+}
