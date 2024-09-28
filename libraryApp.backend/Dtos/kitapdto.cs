bedb1ea3d2d2e478790bca0ae17f0a4f0fdecfbc
zehrakdemir <151151000+zehrakdemir@users.noreply.github.com>
2024-09-28 15:53:46 +0300

    add bookcontroller

---
+++ libraryApp.backend/Dtos/kitapdto.cs
@@ -0,0 +1,12 @@
+﻿namespace libraryApp.backend.Dtos
+{
+    public class kitapdto
+    {
+        public int Id { get; set; }
+        public string kitapIsmi { get; set; } = string.Empty;
+        public DateTime yayinlanmaTarihi { get; set; }
+        public bool oduncAlindiMi { get; set; }
+
+        public List<string> kitapYazarlari { get; set; } = new();
+    }
+}
